51f327d89c73983a2cfd3dcae0e8508daa15a698
JuanFernandoUribeRocha <fernandouribe.tech@gmail.com>
2026-06-04 18:18:31 -0700

    Se agrega prueba para extraer textos de los h2 de la página y contabilizarlos

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,29 +1,41 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("has title", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("get started link", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole("link", { name: "Get started" }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(
+    page.getByRole("heading", { name: "Installation" }),
+  ).toBeVisible();
 
   await page.pause();
 });
 
-test ('testAmazon', async ({page}) => {
+test("testAmazon", async ({ page }) => {
+  await page.goto("https://www.amazon.com.mx");
+  await page.locator("input[id='twotabsearchtextbox']").fill("Iphone 17");
+  await page.keyboard.press("Enter");
+  await expect(
+    page.locator("//span[contains(@class, 'a-button-text a-declarative')]"),
+  ).toBeVisible();
+  //await page.pause();
+  const titles = await page
+    .locator("//h2[contains(@class, \'a-size-base-plus\')]")
+    .allInnerTexts();
 
-  await page.goto('https://www.amazon.com.mx');
-  await page.locator('input[id=\'twotabsearchtextbox\']').fill('Iphone 17');
-  await page.keyboard.press('Enter');
-  await expect(page.locator('//span[contains(@class, \'a-button-text a-declarative\')]')).toBeVisible();
-  await page.pause();
+  console.log("El total de resultados es: ", titles.length);
+
+  for (let title of titles) {
+    console.log("El titulo es: ", title);
+  }
 });
